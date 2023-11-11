57b11f726b58f54a7cd7db564013084ed1372263
Egorkka74 <Egorkka7401@gmail.com>
2023-11-11 12:28:30 +0500

    Init project

---
+++ src/test/java/com/example/socialratingapi/SocialRatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialratingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRatingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
